4c600d50ed1176cd4a94f44b722511ef68e473f8
Vipul Gupta <117650566+1o28@users.noreply.github.com>
2023-03-26 15:32:03 +0530

    Update BinarySearchP2.java

--- BinarySearchP2.java
+++ BinarySearchP2.java
@@ -20,7 +20,6 @@ public class BinarySearchP2 {
         System.out.print("Enter the element which we need to find: ");
         int target = sc.nextInt();
         sc.close();
-        // System.out.println(n-1);
         int index = binarySearch(arr, 0, (n-1), target);
         if(index == -1){
             System.out.println("Element not present in the array.");
