a1a36cbf0b4d5faaae42ff0aa2bbbd363e33f5d3
Vipul Gupta <117650566+1o28@users.noreply.github.com>
2023-03-26 12:19:43 +0530

    Update BinarySearchP1.java

--- BinarySearchP1.java
+++ BinarySearchP1.java
@@ -1,3 +1,7 @@
+/*
+Program to find the target element using binary search algorithm.
+
+*/
 import java.util.*;
 class BinarySearchP1{
     public static int binarySearch(int[] arr, int start, int end, int target){
@@ -19,18 +23,17 @@ class BinarySearchP1{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter size of array: ");
-        int n = sc.nextInt();
+        int n = sc.nextInt();   // Taking array size input from user
         System.out.print("Enter elements of array: ");
-        int[] arr = new int[n];
-        for(int i=0; i<n; i++){
+        int[] arr = new int[n]; // Initialize array 
+        for(int i=0; i<n; i++){ // Taking array element from user
             arr[i] = sc.nextInt();
         }
         System.out.print("Enter the element which we need to find: ");
-        int target = sc.nextInt();
+        int target = sc.nextInt(); // Taking target element which we need to find
         sc.close();
-        // System.out.println(n-1);
         int index = binarySearch(arr, 0, (n-1), target);
-        if(index == -1){
+        if(index == -1){  // If in case element not present in the array our binary search func return -1, 
             System.out.println("Element not present in the array.");
         }
         else{
