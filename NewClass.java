0802c281e386909487d8b3bd8c6e6121e16b5efa
Vipul Gupta <mail.vipulg@gmail.com>
2023-05-13 23:46:46 +0530

    checking some functionalities, learning git

---
+++ NewClass.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class NewClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        System.out.println(n);
+    }
+}
